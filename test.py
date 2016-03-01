f51de8489e323016afcc5f3c46a86056d4205fc5
jlee58 <ba911y2@gmail.com>
2016-03-01 14:11:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-def getContent(url):
+def getContent(url,pid):
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept':'text/html;q=0.9,*/*;q=0.8','Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding':'gzip','Connection':'close'}
     res = requests.get(url, verify=False, headers=headers)
+    soup = BeautifulSoup(res.text,'html.parser')
+    #print soup.text
+    t = soup.find('td', id=re.compile("postmessage_\d+")).text
+    date = soup.find('em', id=re.compile("authorposton\d+")).text
+    author = soup.find('a', class_="author-id").text
+   
+    print pid, author, date, t
 
 def getPage():
     
@@ -15,7 +23,7 @@ def getPage():
     a = soup.findAll("a", class_="s")
     #print res.request.headers
     
-    c = 8
+    c = 5
     for link in a:
         surl = link.get('href')
         c = c - 1
@@ -23,6 +31,7 @@ def getPage():
             break
         if not re.search(r'thread-51', surl) and not re.search(r'thread-160193', surl) and not re.search(r'thread-50261', surl):
             print surl
-            getContent('http://www.citytalk.tw/bbs'+surl)
+            pid = re.match(r'thread-(\d+)',surl).group(1)
+            getContent('http://www.citytalk.tw/bbs/'+surl,pid)
             
 getPage()
